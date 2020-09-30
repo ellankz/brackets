2a6f6e6b2c27f0219018efd332e01edbeb5ceff2
ellankz <enelipa@gmail.com>
2020-09-30 18:49:04 +0300

    feat: solved brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let copyStr = str;
+    const haveMatches = (bracketPair) => {
+        return copyStr.includes(bracketPair.join(""));
+    };
+    while (bracketsConfig.some(haveMatches)) {
+        bracketsConfig.forEach((configPair) => {
+            const pairStr = configPair.join("");
+            while (copyStr.includes(pairStr)) {
+                copyStr = copyStr.replace(pairStr, "");
+            }
+        });
+    }
+    return copyStr.length === 0;
+};
